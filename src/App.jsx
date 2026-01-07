088a020d5d79d65cdf5de8b8ccb20a2483eb8b08
samridhi8595-lab <samridhi8595@gmail.com>
2026-01-07 20:08:53 +0530

    basic nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
+import Product from './pages/Product'
 import {Route,Routes} from 'react-router-dom'
 import NotFound from './pages/NotFound'
+import Men from './pages/Men'
+import Women from './pages/Women'
 const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
@@ -13,7 +15,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/product/men' element={<Men/>}/>
+        <Route path='/product/women' element={<Women/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
       <Footer/>
